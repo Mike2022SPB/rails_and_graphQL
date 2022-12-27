6a0d133e3da68f00ea594b3a0f03d66cc0499eca
Mike2022SPB <mike.godunoff2020@mail.ru>
2022-12-27 12:21:29 +0400

    The code has been changed in accordance with the manual, paragraph 'Dealing with cache' V2

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -12,7 +12,6 @@ class GraphqlController < ApplicationController
 
   private
 
-  # Handle form data, JSON body, or a blank value
   def ensure_hash(ambiguous_param)
     case ambiguous_param
     when String
