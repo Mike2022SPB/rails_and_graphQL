81856430c1d76c113a5df5be74332094390feb0b
Mike2022SPB <mike.godunoff2020@mail.ru>
2022-12-28 11:49:35 +0400

    Laying the cable

--- app/graphql/graph_q_lproject_schema.rb
+++ app/graphql/graph_q_lproject_schema.rb
@@ -1,6 +1,7 @@
 class GraphQLprojectSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
+  subscription(Types::SubscriptionType)
 
   # For batch-loading (see https://graphql-ruby.org/dataloader/overview.html)
   use GraphQL::Dataloader
